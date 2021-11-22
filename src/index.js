b13013959a882e55f85237f0b41a2140a3194f2f
SerhiiLytviak <litvyak.sergey@gmail.com>
2021-11-22 15:24:51 +0200

    Пройдено занятие по настройке Babel и Parcel
    Настраивал Babel  и Parcel

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import validatePassword from "./js/validate-password";
+import { addUser } from "./js/api-service";
+
+addUser("mango");
+
+// import { fetchAllUsers, fetchUserById, updateUserById } from "./js/api-service";
+// console.log(validatePassword);
+
+// import * as apiService from "./js/api-service";
+
+// console.log(validatePassword("qweqwe"));
+
+// console.log(fetchAllUsers);
+// console.log(fetchUserById);
+// console.log(updateUserById);
+
+// console.log(apiService);
